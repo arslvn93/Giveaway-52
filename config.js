a1cb70be5ae5737d319055cc4466b94c68961814
arslvn93 <58179630+arslvn93@users.noreply.github.com>
2025-07-23 16:41:42 -0400

    Update Site

--- config.js
+++ config.js
@@ -152,7 +152,7 @@ const config = {
   },
   "modalQuestions": [
     {
-      "id": "q_auto_1753303297176_0",
+      "id": "q_auto_1753303301623_0",
       "questionText": "How frequently do you and your family enjoy participating in community events or exploring local businesses and attractions?",
       "options": [
         {
@@ -174,7 +174,7 @@ const config = {
       ]
     },
     {
-      "id": "q_auto_1753303297176_1",
+      "id": "q_auto_1753303301623_1",
       "questionText": "What are some of your favorite weekend activities with your family?",
       "options": [
         {
@@ -196,7 +196,7 @@ const config = {
       ]
     },
     {
-      "id": "q_auto_1753303297176_2",
+      "id": "q_auto_1753303301623_2",
       "questionText": "When it comes to family outings, what do you value most?",
       "options": [
         {
@@ -218,7 +218,7 @@ const config = {
       ]
     },
     {
-      "id": "q_auto_1753303297176_3",
+      "id": "q_auto_1753303301623_3",
       "questionText": "How do you typically find out about upcoming community events or local promotions?",
       "options": [
         {
