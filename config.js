4b86ac62d805568d3e0cec8af59d2281706196b1
arslvn93 <58179630+arslvn93@users.noreply.github.com>
2025-07-24 11:35:13 -0400

    Update Site

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ const config = {
     "privacyPolicyLink": "https://driscollpeca.com/privacy.html"
   },
   "giveaway": {
-    "heroHeadline": "Join the <span class='highlight'>Amazing Race Kemptville</span> for a Chance to Win <span class='highlight'>$800+ in Prizes!</span>",
+    "heroHeadline": "Join the <span class='highlight'> Amazing Race Kemptville </span> for a Chance to Win <span class='highlight'> $800+ in Prizes! </span>",
     "heroSubheadline": "Explore local shops, finish fun challenges, and you could win a huge Back-to-School Family Pack or Summer Fun Pack. Registration is open now—don’t miss out!",
     "heroBackgroundImageUrl": "https://i.ibb.co/SD1J78Ln/1.png",
     "promotionDates": "Enter between July 24 and August 15. Winner drawn August 19, 2025.",
@@ -152,7 +152,7 @@ const config = {
   },
   "modalQuestions": [
     {
-      "id": "q_auto_1753371207224_0",
+      "id": "q_auto_1753371313176_0",
       "questionText": "How frequently do you and your family enjoy participating in community events or exploring local businesses and attractions?",
       "options": [
         {
@@ -174,7 +174,7 @@ const config = {
       ]
     },
     {
-      "id": "q_auto_1753371207224_1",
+      "id": "q_auto_1753371313176_1",
       "questionText": "What are some of your favorite weekend activities with your family?",
       "options": [
         {
@@ -196,7 +196,7 @@ const config = {
       ]
     },
     {
-      "id": "q_auto_1753371207224_2",
+      "id": "q_auto_1753371313176_2",
       "questionText": "When it comes to family outings, what do you value most?",
       "options": [
         {
@@ -218,7 +218,7 @@ const config = {
       ]
     },
     {
-      "id": "q_auto_1753371207224_3",
+      "id": "q_auto_1753371313176_3",
       "questionText": "How do you typically find out about upcoming community events or local promotions?",
       "options": [
         {
