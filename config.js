7116f3e445e2db3531fb417729e66bd64e8f8e96
arslvn93 <58179630+arslvn93@users.noreply.github.com>
2025-07-24 11:33:24 -0400

    Update Site

--- config.js
+++ config.js
@@ -25,7 +25,7 @@ const config = {
     ],
     "showCountdownInHero": true,
     "theme": "dark",
-    "primaryColorOverride": "",
+    "primaryColorOverride": "#0fc3f8",
     "ghlWebhookUrl": "",
     "facebookPixelId": "1057475448873422",
     "salesGeniusAppApi": "",
@@ -152,7 +152,7 @@ const config = {
   },
   "modalQuestions": [
     {
-      "id": "q_auto_1753368437918_0",
+      "id": "q_auto_1753371203257_0",
       "questionText": "How frequently do you and your family enjoy participating in community events or exploring local businesses and attractions?",
       "options": [
         {
@@ -174,7 +174,7 @@ const config = {
       ]
     },
     {
-      "id": "q_auto_1753368437918_1",
+      "id": "q_auto_1753371203257_1",
       "questionText": "What are some of your favorite weekend activities with your family?",
       "options": [
         {
@@ -196,7 +196,7 @@ const config = {
       ]
     },
     {
-      "id": "q_auto_1753368437918_2",
+      "id": "q_auto_1753371203257_2",
       "questionText": "When it comes to family outings, what do you value most?",
       "options": [
         {
@@ -218,7 +218,7 @@ const config = {
       ]
     },
     {
-      "id": "q_auto_1753368437918_3",
+      "id": "q_auto_1753371203257_3",
       "questionText": "How do you typically find out about upcoming community events or local promotions?",
       "options": [
         {
