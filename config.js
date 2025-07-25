bbad986b9492113f23918e9dcd9381f31bc50735
arslvn93 <58179630+arslvn93@users.noreply.github.com>
2025-07-25 17:18:02 -0400

    Update Site

--- config.js
+++ config.js
@@ -39,7 +39,7 @@ const config = {
         "alt": "Family having fun"
       },
       {
-        "src": "https://ibb.co/fdxP989x",
+        "src": "https://i.ibb.co/4gss0mCy/10-1.png",
         "alt": "Teens smiling outside a shop in Kemptville"
       },
       {
@@ -173,7 +173,7 @@ const config = {
   },
   "modalQuestions": [
     {
-      "id": "q_auto_1753477480179_0",
+      "id": "q_auto_1753478281634_0",
       "questionText": "Are you a Kemptville Area Resident?",
       "options": [
         {
