5320b33ec349c9759f0ae0454de17e47efcf72b3
arslvn93 <58179630+arslvn93@users.noreply.github.com>
2025-07-25 15:21:12 -0400

    Update Site

--- config.js
+++ config.js
@@ -173,7 +173,7 @@ const config = {
   },
   "modalQuestions": [
     {
-      "id": "q_auto_1753471130601_0",
+      "id": "q_auto_1753471270760_0",
       "questionText": "What do you like the most about your current home?",
       "options": [
         {
@@ -195,7 +195,7 @@ const config = {
       ]
     },
     {
-      "id": "q_auto_1753471130601_1",
+      "id": "q_auto_1753471270760_1",
       "questionText": "Is a move on your radar in the near future?",
       "options": [
         {
@@ -217,7 +217,7 @@ const config = {
       ]
     },
     {
-      "id": "q_auto_1753471130601_2",
+      "id": "q_auto_1753471270760_2",
       "questionText": "What’s your current housing situation?",
       "options": [
         {
@@ -258,7 +258,7 @@ const config = {
     },
     "agentContact": {
       "name": "Driscoll-Peca Team",
-      "title": "Real Estate Agent",
+      "title": "Real Estate Team",
       "phone": "(613) 706-1616",
       "email": "hello@driscollpeca.com"
     },
